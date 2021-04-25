a5fe4ff26be91d5eeb54bdb97841e43406bf7f91
z5122476 <z5122476@student.unsw.edu.au>
2021-04-25 17:40:56 +1000

    updated removeStack to version in solution.yml

--- removeStack.py
+++ removeStack.py
@@ -1,23 +1,27 @@
 import json
-import boto3
-client = boto3.client('cloudformation')
+          import boto3
+          import os
+          client = boto3.client('cloudformation')
 
-def lambda_handler(event, context):
-    # TODO implement
-    stackName = event["id"]
-    print(stackName)
-    capabilities = ['CAPABILITY_IAM']
-    roleArn = "arn:aws:iam::221094580673:role/service-role/CreateStack-role-9vv4yuf8"
-    resourceTypes = ['AWS::*']
-    
-    
-    response = client.delete_stack(
-        StackName=stackName,
-        RoleARN=roleArn
-    )
-    
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+          def lambda_handler(event, context):
+              # TODO implement
+              stackName = event["id"]
+              print(stackName)
+              capabilities = ['CAPABILITY_IAM']
+              lambda_client = boto3.client('lambda')
+              role_response = (lambda_client.get_function_configuration(
+                  FunctionName = os.environ['AWS_LAMBDA_FUNCTION_NAME'])
+              )
+              print(role_response)
+              roleArn = role_response['Role']
+              resourceTypes = ['AWS::*']
+              
+              response = client.delete_stack(
+                  StackName=stackName,
+                  RoleARN=roleArn
+              )
+              
+              return {
+                  'statusCode': 200,
+                  'body': json.dumps('Hello from Lambda!')
+              }
